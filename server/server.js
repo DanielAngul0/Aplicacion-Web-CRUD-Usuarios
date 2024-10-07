cd9ab40a5f38284cfe798d0fe29280cfca4f8412
DanielAngul0 <daniel.andres.angulo.perez>
2024-10-07 09:55:29 -0500

    creando configuracion principal y global de 'server.js' para todos los archivos que dependan el, ahora existe un servidor, que esta a la escucha en el puerto 3000

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// ----- 'server.js' es el archivo principal en el cual se configura todo el servidor, por lo cual todos los middeware que se usen y las configuraciones que tenga este archivo afectara a todos los demas archivos a los que 'server.js' les tenga configuracion o se vinculen a el -----
+
+
+// importando el framework 'express'
+import express from 'express'
+// importa todas las rutas del CRUD de la carpeta '../rutas/usuarios.js' para que sea accesibles bajo la direccion '/usuarios'
+import usuariosRouter from '../rutas/usuarios.js'
+
+// creando instancia de la aplicacion 'express'
+const app = express();
+// Creando puerto 3000
+const port = 3000;
+
+// Este middleware ayuda a express a interpretar codigo en formato JSON, util para los metodos HTTP que seran comprendidos a traves de req.body manejando los datos del cliente
+app.use(express.json());
+// Este middleware es esencial para que cualquier archivo de la carpeta 'Public' sea accesible desde la URL: 'http://localhost:3000/', es decir; 'public/index.html' ahora sera la pagina principal
+app.use(express.static('../public'));
+// Todas las rutas definidas en 'usuariosRouter' estarán bajo el prefijo '/usuarios'. Esto significa que, al hacer una solicitud a '/usuarios', esa solicitud será gestionada por 'usuariosRouter', que se encargará de manejar las operaciones del CRUD y las rutas relacionadas con los usuarios.
+app.use('/usuarios', usuariosRouter);
+
+// Servidor a la escucha en el puerto 3000
+app.listen(port , () =>{
+    console.log(`Servidor a la escucha en el puerto: ${port}`);
+})
